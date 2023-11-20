f1af6cdd1639e6a4ff886a9b1ef15fae18288c41
Jclyoung <jclyoung@gmail.com>
2023-11-20 13:58:05 -0600

    mend

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,13 @@
 import './index.scss'
 import { Link } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoJ from '../../assets/images/logo-j.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => {
   <div className='nav-bar'>
     <Link className='logo' to='/'>
-      <img src={LogoS} alt='logo' />
-      <img src={LogoSubtitle} alt='young' />
+      <img src={LogoJ} alt='logo' />
+      <img src={LogoSubtitle} alt='jennifer young' />
     </Link>
   </div>
 }
